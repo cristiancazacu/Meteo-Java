8a0ae76e782aecf8c0027efffa5f11ee1b519908
Cazacu Cristian <cazacucristian27@gmail.com>
2021-01-12 11:48:58 +0200

    UI COMMIT: Interfata Grafica varianta 1.0 pentru aplicatia Meteo cu fereastra nr. 1 Urmeaza si o fereastra nr 2 pentru cele 2 liste de selectie tari si orase sau o integrare acestora in fereastra nr. 1.
    Signed-off by: Cristian Cazacu <cazacucristian27@gmail.com>

--- src/ro/mta/se/lab/Controller.java
+++ src/ro/mta/se/lab/Controller.java
@@ -1,4 +1,13 @@
 package ro.mta.se.lab;
 
+import javafx.fxml.FXML;
+
+import java.awt.event.MouseEvent;
+
 public class Controller {
+
+    public void close_button(javafx.scene.input.MouseEvent mouseEvent) {
+        System.exit(0);
+    }
+
 }
--- src/ro/mta/se/lab/Main.java
+++ src/ro/mta/se/lab/Main.java
@@ -1,18 +1,26 @@
 package ro.mta.se.lab;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import javax.swing.text.html.ImageView;
 
 public class Main extends Application {
+
+    @FXML
+    ImageView exitButtonImage;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 400, 380));
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
         primaryStage.show();
     }
 
