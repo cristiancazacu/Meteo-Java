f6f0fe4d22c55bcde1f244893c65aa9d98c4dc09
Cazacu Cristian <cazacucristian27@gmail.com>
2021-01-13 21:13:29 +0200

    UI COMMIT 2: Interfata Grafica varianta 2.0 pentru aplicatia Meteo cu fereastra nr. 1. Implementarea butoanelor cu animatii si a evenimentelor dar si a elementelor (in forma statica) demo ce urmeaza sa fie populate de catre API.
    Urmeaza si o fereastra nr 2 pentru cele 2 liste de selectie tari si orase sau
    o integrare acestora in fereastra nr. 1.
    
    Signed-off by: Cristian Cazacu <cazacucristian27@gmail.com>

--- src/ro/mta/se/lab/Controller.java
+++ src/ro/mta/se/lab/Controller.java
@@ -1,13 +1,44 @@
 package ro.mta.se.lab;
 
 import javafx.fxml.FXML;
-
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import org.jetbrains.annotations.NotNull;
+//import javax.swing.text.html.ImageView;
 import java.awt.event.MouseEvent;
 
+
+
 public class Controller {
+    @FXML
+    public ImageView exitButtonImage;
+    @FXML
+    public ImageView refreshButtonImage;
+
+   // Controller(Parent scene){ this.scene = scene;}
+
 
     public void close_button(javafx.scene.input.MouseEvent mouseEvent) {
         System.exit(0);
     }
+
+    public static Node findItemById(Parent root, @NotNull String ID)
+    {
+        Node n;
+        if (ID.contains("#"))
+        {
+             n = root.lookup(ID);
+            return n;
+        }
+        else {
+            ID = '#' + ID;
+            n = root.lookup(ID);
+            return n;
+        }
+    }
+
+
 
 }
