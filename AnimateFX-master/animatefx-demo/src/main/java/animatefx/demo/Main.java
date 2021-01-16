a89b9fe6e44e373eca4c4564be156380e146a404
Abhinay Agarwal <abhinayagarwal@live.com>
2021-01-16 18:11:45 +0200

    UI COMMIT FINAL: Interfata Grafica varianta finala pentru aplicatia Meteo cu fereastra nr. 1 + animatii folosind AnimateFX si metode native JavaFX precum RotateTranslate sau TranslateTransition prin intermediul clasei Wrapper EffectsHandler.java. Implementarea ferestrei (paginii) nr 2 selector.fxml prin tranzitia din container parinte (AnchorPane) la celalat fisier .fxml. Implementare minimala de evenimente.
    Signed-off by: Cristian Cazacu <cazacucristian27@gmail.com>

--- AnimateFX-master/animatefx-demo/src/main/java/animatefx/demo/Main.java
+++ AnimateFX-master/animatefx-demo/src/main/java/animatefx/demo/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/mainWindow.fxml"));
         primaryStage.setTitle("AnimateFX Demo");
         primaryStage.setScene(new Scene(root, 700, 700));
         primaryStage.show();
--- src/ro/mta/se/lab/Main.java
+++ src/ro/mta/se/lab/Main.java
@@ -1,25 +1,16 @@
 package ro.mta.se.lab;
 
-import animatefx.animation.*;
-import javafx.animation.RotateTransition;
 import javafx.application.Application;
-import javafx.beans.value.ChangeListener;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
-import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-import javafx.scene.image.ImageView;
-import javafx.util.Duration;
-import ro.mta.se.lab.image.EffectsHandler;
+import ro.mta.se.lab.Controllers.Controller;
 
-import java.awt.*;
+
 import java.io.IOException;
 
 public class Main extends Application {
@@ -28,7 +19,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml")); //save instance of Controller
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Views/mainWindow.fxml")); //save instance of Controller
         Parent root;
 
 
@@ -42,7 +33,7 @@ public class Main extends Application {
         Controller ctrl = (Controller) loader.getController();
 
         primaryStage.setTitle("Meteo");     // Titlu App
-        primaryStage.setScene(new Scene(root, 400, 380));   // inaltime: 400, latime: 380
+        primaryStage.setScene(new Scene(root, 399, 379));   // latime: 399, inaltime: 379
         primaryStage.initStyle(StageStyle.TRANSPARENT);         // dispar colturi albe
         primaryStage.getScene().setFill(Color.TRANSPARENT);     // dispar colturi albe
         primaryStage.show();
@@ -53,10 +44,6 @@ public class Main extends Application {
         EffectsHandler.PlayImageRotationTranslate(ctrl.refreshButtonImage, 0.5,0,185,true,1);
         EffectsHandler.PlayImageButtonSelected(ctrl.exitButtonImage);
 
-
-        //refreshButtonImage = (ImageView) root.lookup("#refreshButtonImage");
-        //Node uiobject = refreshButtonImage;
-        //Node element = root;
 
 
     }
